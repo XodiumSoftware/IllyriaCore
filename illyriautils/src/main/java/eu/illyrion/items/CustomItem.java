9bf6e6fc93dc4951d357efa4049acb0d7027aafc
Your Name <you@example.com>
2024-07-15 18:15:44 +0000

    feat: Add RecipeShapeless and RecipeShaped classes

--- illyriautils/src/main/java/eu/illyrion/items/CustomItem.java
+++ illyriautils/src/main/java/eu/illyrion/items/CustomItem.java
@@ -3,12 +3,8 @@ package eu.illyrion.items;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import eu.illyrion.utils.Utils;
@@ -20,14 +16,9 @@ public class CustomItem {
      * Initializes the custom items by creating the necessary recipes.
      */
     public static void init() {
-        ItemStack condensedPotato = createCustomItem(Material.POTATO, "{#FFA500}Condensed Potato",
+        createCustomItem(Material.POTATO, "{#FFA500}Condensed Potato",
                 Arrays.asList("{#FFFFFF}This is a condensed potato made from 9 potatoes",
                         "{#FFFFFF}Another line of lore"));
-
-        createRecipe(
-                new RecipeShaped(new ItemStack(Material.POTATO), condensedPotato, 1,
-                        Arrays.asList("PPP", "PPP", "PPP")),
-                new RecipeShapeless(condensedPotato, new ItemStack(Material.POTATO), 9));
     }
 
     /**
@@ -57,31 +48,4 @@ public class CustomItem {
         return item;
     }
 
-    /**
-     * Creates a recipe for a given item.
-     *
-     * @param shaped    The shaped recipe object.
-     * @param shapeless The shapeless recipe object.
-     */
-    private static void createRecipe(RecipeShaped shaped, RecipeShapeless shapeless) {
-        if (shaped != null) {
-            ItemStack inputItem = shaped.inputItem;
-            ItemStack outputItem = new ItemStack(shaped.outputItem.getType(), shaped.outputQuantity);
-            List<String> shape = shaped.shape;
-            ShapedRecipe shapedRecipe = new ShapedRecipe(
-                    new NamespacedKey(namespace, outputItem.getType().toString().toLowerCase()), outputItem);
-            shapedRecipe.shape(shape.toArray(new String[0]));
-            shapedRecipe.setIngredient('P', inputItem.getType());
-            Bukkit.addRecipe(shapedRecipe);
-        }
-
-        if (shapeless != null) {
-            ItemStack inputItem = shapeless.inputItem;
-            ItemStack outputItem = new ItemStack(shapeless.outputItem.getType(), shapeless.outputQuantity);
-            ShapelessRecipe shapelessRecipe = new ShapelessRecipe(
-                    new NamespacedKey(namespace, outputItem.getType().toString().toLowerCase()), outputItem);
-            shapelessRecipe.addIngredient(inputItem.getType());
-            Bukkit.addRecipe(shapelessRecipe);
-        }
-    }
 }
