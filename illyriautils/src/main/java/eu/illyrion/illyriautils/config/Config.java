bb47ae72cd76f0bc42e4479592007fec7d09203b
illyrius666 <FitimQ@live.nl>
2024-07-17 11:27:44 +0000

    + moved everything to config.java + updated pom dependency to newest version

--- illyriautils/src/main/java/eu/illyrion/illyriautils/config/Config.java
+++ illyriautils/src/main/java/eu/illyrion/illyriautils/config/Config.java
@@ -1,29 +1,42 @@
 package eu.illyrion.illyriautils.config;
 
-import org.bukkit.configuration.file.FileConfiguration;
-
 import eu.illyrion.illyriautils.IllyriaUtils;
 
 import org.bstats.bukkit.Metrics;
+import org.bukkit.configuration.file.FileConfiguration;
 
 public class Config {
 
-    public static final String CHAT_PREFIX = "prefix";
-    public static final String DEBUG = "debug";
-    public static final String CHECK_FOR_UPDATES = "check-for-updates";
-    public static final String CHECK_FOR_UPDATES_INTERVAL = "check-interval";
+    public static final String GENERAL_PREFIX = "general.";
+    public static final String CHAT_PREFIX = GENERAL_PREFIX + "prefix";
+    public static final String CHECK_FOR_UPDATES = GENERAL_PREFIX + "check-for-updates";
+    public static final String CHECK_FOR_UPDATES_INTERVAL = GENERAL_PREFIX + "check-interval";
 
-    private static final int SERVICE_ID = 22667;
+    private static final String MODULES_PREFIX = "modules.";
+    public static final String CUSTOM_ITEM_HANDLER = MODULES_PREFIX + "CustomItemHandler";
 
+    private static final String DEVELOPMENT_PREFIX = "development.";
+    public static final String DEBUG = DEVELOPMENT_PREFIX + "debug";
+
+    private static final int METRICS_SERVICE_ID = 22667;
+
+    /**
+     * Initializes the configuration for the IllyriaUtils plugin.
+     * This method sets default values for various configuration options and
+     * initializes metrics.
+     */
     public static void init() {
         IllyriaUtils plugin = IllyriaUtils.getInstance();
         FileConfiguration conf = plugin.getConfig();
-        conf.addDefault(CHAT_PREFIX, "[IllyriaUtils] ");
-        conf.addDefault(DEBUG, false);
+
+        conf.addDefault(CHAT_PREFIX, "[IllyriaUtils]");
         conf.addDefault(CHECK_FOR_UPDATES, true);
         conf.addDefault(CHECK_FOR_UPDATES_INTERVAL, 4);
 
-        new Metrics(plugin, SERVICE_ID);
-    }
+        conf.addDefault(CUSTOM_ITEM_HANDLER, true);
 
+        conf.addDefault(DEBUG, false);
+
+        new Metrics(plugin, METRICS_SERVICE_ID);
+    }
 }
