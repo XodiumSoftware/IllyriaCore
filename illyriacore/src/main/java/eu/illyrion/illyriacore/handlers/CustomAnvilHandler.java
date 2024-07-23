d2126892e8b5fc99ea52dbb7412564be135883ae
illyrius666 <FitimQ@live.nl>
2024-07-23 09:01:26 +0000

    + Update IllyriaCore version to 0.0.12-SNAPSHOT + refactor PrepareAnvilEvent for enchantments

--- illyriacore/src/main/java/eu/illyrion/illyriacore/handlers/CustomAnvilHandler.java
+++ illyriacore/src/main/java/eu/illyrion/illyriacore/handlers/CustomAnvilHandler.java
@@ -1,6 +1,7 @@
 package eu.illyrion.illyriacore.handlers;
 
 import java.util.Iterator;
+import java.util.Optional;
 
 import org.bukkit.Material;
 import org.bukkit.Registry;
@@ -14,34 +15,55 @@ import org.bukkit.inventory.ItemStack;
 public class CustomAnvilHandler implements Listener {
 
     /**
-     * Handles the PrepareAnvilEvent.
-     * This method is called when an anvil is being prepared for use.
+     * Handles the PrepareAnvilEvent, which is triggered when a player prepares an
+     * anvil.
+     * This method checks if the first and second items in the anvil inventory are
+     * enchanted books.
+     * If they are, it iterates through all enchantments and checks if the minimum
+     * level of the enchantment
+     * in the first item is equal to the maximum level of the enchantment in the
+     * second item.
+     * If the conditions are met, it creates a new ItemStack with an increased level
+     * of the enchantment
+     * and sets it as the result of the event.
      *
-     * @param e The PrepareAnvilEvent object containing information about the event.
+     * @param e The PrepareAnvilEvent object representing the event.
      */
     @EventHandler
     public void onPrepareAnvil(PrepareAnvilEvent e) {
         AnvilInventory anvilInventory = e.getInventory();
-        ItemStack firstItem = anvilInventory.getItem(0);
-        ItemStack secondItem = anvilInventory.getItem(1);
-        if (firstItem != null && secondItem != null && firstItem.getType() == Material.ENCHANTED_BOOK
-                && secondItem.getType() == Material.ENCHANTED_BOOK) {
-            Iterator<Enchantment> enchantments = Registry.ENCHANTMENT.iterator();
-            while (enchantments.hasNext()) {
-                Enchantment enchantment = enchantments.next();
-                if (enchantment.getMaxLevel() > 1) {
-                    int minLVL = firstItem.getEnchantmentLevel(enchantment);
-                    int maxLVL = secondItem.getEnchantmentLevel(enchantment);
-                    int vanillaMaxLevel = enchantment.getMaxLevel();
-                    int customMaxLevel = vanillaMaxLevel + 5;
-                    if (minLVL == maxLVL && minLVL >= 1 && minLVL < customMaxLevel) {
-                        ItemStack result = new ItemStack(Material.ENCHANTED_BOOK);
-                        result.addUnsafeEnchantment(enchantment, minLVL + 1);
-                        e.setResult(result);
-                        break;
-                    }
-                }
+        Optional<ItemStack> firstItem = Optional.ofNullable(anvilInventory.getItem(0));
+        Optional<ItemStack> secondItem = Optional.ofNullable(anvilInventory.getItem(1));
+
+        if (!firstItem.isPresent() || !secondItem.isPresent()) {
+            return;
+        }
+
+        if (firstItem.get().getType() != Material.ENCHANTED_BOOK
+                || secondItem.get().getType() != Material.ENCHANTED_BOOK) {
+            return;
+        }
+
+        Iterator<Enchantment> enchantments = Registry.ENCHANTMENT.iterator();
+        while (enchantments.hasNext()) {
+            Enchantment enchantment = enchantments.next();
+            if (enchantment.getMaxLevel() <= 1) {
+                continue;
             }
+
+            int minLVL = firstItem.get().getEnchantmentLevel(enchantment);
+            int maxLVL = secondItem.get().getEnchantmentLevel(enchantment);
+            int vanillaMaxLevel = enchantment.getMaxLevel();
+            int customMaxLevel = vanillaMaxLevel + 5;
+
+            if (minLVL != maxLVL || minLVL < 1 || minLVL >= customMaxLevel) {
+                continue;
+            }
+
+            ItemStack result = new ItemStack(Material.ENCHANTED_BOOK);
+            result.addUnsafeEnchantment(enchantment, minLVL + 1);
+            e.setResult(result);
+            break;
         }
     }
 }
