d10a2c1102f1377a3fadb730726490154e838af8
illyrius666 <FitimQ@live.nl>
2024-07-25 10:36:37 +0000

    + fixed ImmunityHandlerTest

--- illyriacore/src/test/java/eu/illyrion/illyriacore/handlers/ImmunityHandlerTest.java
+++ illyriacore/src/test/java/eu/illyrion/illyriacore/handlers/ImmunityHandlerTest.java
@@ -1,49 +1,53 @@
 package eu.illyrion.illyriacore.handlers;
 
-// import be.seeseemelk.mockbukkit.MockBukkit;
-// import be.seeseemelk.mockbukkit.entity.PlayerMock;
-// import eu.illyrion.illyriacore.IllyriaCore;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.AfterEach;
-// import org.junit.jupiter.api.Test;
-// import static org.junit.jupiter.api.Assertions.assertFalse;
-// import static org.junit.jupiter.api.Assertions.assertTrue;
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import be.seeseemelk.mockbukkit.scheduler.BukkitSchedulerMock;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import eu.illyrion.illyriacore.IllyriaCore;
+import eu.illyrion.illyriacore.configs.Config;
+import net.kyori.adventure.text.Component;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ImmunityHandlerTest {
 
-    // private IllyriaCore plugin;
+    private IllyriaCore plugin;
+    private ImmunityHandler immunityHandler;
+    private BukkitSchedulerMock scheduler;
+    private FileConfiguration config;
 
-    // @BeforeEach
-    // public void setUp() {
-    // MockBukkit.mock();
-    // plugin = MockBukkit.load(IllyriaCore.class);
-    // plugin.getServer().getPluginManager().registerEvents(new ImmunityHandler(),
-    // MockBukkit.createMockPlugin());
-    // }
+    @BeforeEach
+    public void setUp() {
+        MockBukkit.mock();
+        plugin = MockBukkit.load(IllyriaCore.class);
+        scheduler = MockBukkit.getMock().getScheduler();
+        immunityHandler = new ImmunityHandler();
+        PluginManager pluginManager = MockBukkit.getMock().getPluginManager();
+        pluginManager.registerEvents(immunityHandler, plugin);
+        config = Config.init();
+        config.set("ImmunityTimerDuration", 10);
+    }
 
-    // @AfterEach
-    // public void tearDown() {
-    // MockBukkit.unmock();
-    // }
+    @AfterEach
+    public void tearDown() {
+        MockBukkit.unmock();
+    }
 
-    // @Test
-    // public void testMakePlayerInvulnerable() {
-    // PlayerMock player = new PlayerMock(MockBukkit.getMock(), "TestPlayer");
-    // assertFalse(player.isInvulnerable(), "Player should initially be
-    // vulnerable");
-
-    // ImmunityHandler immunityHandler = new ImmunityHandler();
-    // immunityHandler.makePlayerInvulnerable(player);
-
-    // assertTrue(player.isInvulnerable(), "Player should be invulnerable after
-    // method call");
-
-    // int invulnerabilityDuration =
-    // plugin.getConfig().getInt("immunity_timer_duration");
-    // MockBukkit.getMock().getScheduler().performTicks(invulnerabilityDuration *
-    // 20L + 1);
-
-    // assertFalse(player.isInvulnerable(), "Player should be vulnerable after
-    // duration");
-    // }
+    @Test
+    public void testOnPlayerJoin() {
+        PlayerMock player = MockBukkit.getMock().addPlayer();
+        PlayerJoinEvent joinEvent = new PlayerJoinEvent(player, Component.text("TEST"));
+        MockBukkit.getMock().getPluginManager().callEvent(joinEvent);
+        assertTrue(player.isInvulnerable());
+        scheduler.performTicks(config.getInt("ImmunityTimerDuration") * 20 + 1);
+        assertFalse(player.isInvulnerable());
+    }
 }
