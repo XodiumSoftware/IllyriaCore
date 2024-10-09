13bc5c7773a8d4c4bd9d5b34194a4088b7bde07f
illyrius666 <fitimq@live.nl>
2024-10-09 12:54:37 +0000

    Refactor project configuration and dependencies

--- src/main/java/org/xodium/illyriacore/IllyriaCore.java
+++ src/main/java/org/xodium/illyriacore/IllyriaCore.java
@@ -2,12 +2,7 @@ package org.xodium.illyriacore;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import org.xodium.illyriacore.configs.Config;
-import org.xodium.illyriacore.handlers.ModuleHandler;
-import org.xodium.illyriacore.interfaces.MessagesInterface;
-import org.xodium.illyriacore.utils.IllyriaUtils;
-
-public class IllyriaCore extends JavaPlugin implements MessagesInterface {
+public class IllyriaCore extends JavaPlugin {
 
   @Override
   public void onEnable() {
