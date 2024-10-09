c14cb24c03f20fbf1fe80bd5e029d0401c665579
illyrius666 <fitimq@live.nl>
2024-10-09 17:38:19 +0000

    +

--- src/main/java/org/xodium/illyriacore/IllyriaCore.java
+++ src/main/java/org/xodium/illyriacore/IllyriaCore.java
@@ -5,25 +5,28 @@ import java.util.Map;
 
 import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.xodium.illyriacore.interfaces.msg;
+import org.xodium.illyriacore.interfaces.MSG;
 import org.xodium.illyriacore.listeners.EventListener;
 
 import net.luckperms.api.LuckPermsProvider;
 
-public class IllyriaCore extends JavaPlugin implements msg {
+public class IllyriaCore extends JavaPlugin implements MSG {
 
   @Override
   public void onEnable() {
-    getLogger().info(msg.ILLYRIA_CORE_ENABLED);
+    getLogger().info(MSG.ILLYRIA_CORE_ENABLED);
 
     Map<EntityType, String> entityPermMap = new HashMap<>();
-    entityPermMap.put(EntityType.ENDER_DRAGON, "hmcwraps.");
+    entityPermMap.put(EntityType.GUARDIAN, "hmcwraps.guardian_set");
+    entityPermMap.put(EntityType.WITHER, "hmcwraps.wither_set");
+    entityPermMap.put(EntityType.WARDEN, "hmcwraps.warden_set");
+    entityPermMap.put(EntityType.ENDER_DRAGON, "hmcwraps.ender_dragon_set");
 
     getServer().getPluginManager().registerEvents(new EventListener(LuckPermsProvider.get(), entityPermMap), this);
   }
 
   @Override
   public void onDisable() {
-    getLogger().info(msg.ILLYRIA_CORE_DISABLED);
+    getLogger().info(MSG.ILLYRIA_CORE_DISABLED);
   }
 }
