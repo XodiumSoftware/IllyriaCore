220782026cfd6278ad7ac2eb98da93a78fe1918b
illyrius666 <fitimq@live.nl>
2024-10-09 13:46:27 +0000

    +

--- src/main/java/org/xodium/illyriacore/IllyriaCore.java
+++ src/main/java/org/xodium/illyriacore/IllyriaCore.java
@@ -4,12 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.bukkit.entity.EntityType;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.xodium.illyriacore.interfaces.msg;
 import org.xodium.illyriacore.listeners.EventListener;
 
-import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 
 public class IllyriaCore extends JavaPlugin implements msg {
@@ -21,9 +19,7 @@ public class IllyriaCore extends JavaPlugin implements msg {
     Map<EntityType, String> entityPermMap = new HashMap<>();
     entityPermMap.put(EntityType.ZOMBIE, "zombie.permission.node");
 
-    LuckPerms lp = LuckPermsProvider.get();
-    PluginManager pm = getServer().getPluginManager();
-    pm.registerEvents(new EventListener(lp, entityPermMap), this);
+    getServer().getPluginManager().registerEvents(new EventListener(LuckPermsProvider.get(), entityPermMap), this);
   }
 
   @Override
